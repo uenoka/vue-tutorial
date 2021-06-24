0a59e91af60f5c6a1041e0a2becb3376769e7dd1
uenoka <ueno.k.0505@gmail.com>
2021-06-24 23:15:22 +0900

    step 6

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-main.js
+var STRRAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+    fetch: function(){
+        var todos = JSON.parse(
+            localStorag.getItem(STRRAGE_KEY)||'[]'
+        )
+        todos.forEach(
+            function(todo, index){
+                todo.id = index
+            })
+            todoStorage.uid = todos.length
+            return todos
+    },
+    save: function(todos){
+        localStorage.setItem(STRRAGE_KEY,JSON.stringify(todos))
+    }
+}
+const app = new Vue({
+    el:'#app',
+    data:{
+        todos:[
+            { "id": 1, "comment": "新しいToDo1", "state": 3 },
+            { "id": 2, "comment": "新しいToDo2", "state": 0 }          
+        ],
+    },
+    methods:{}
+})
